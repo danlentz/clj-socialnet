059f49f733401b2e37a08ae9177bad1526c0ecb6
Dan Lentz <danlentz@gmail.com>
2015-12-05 12:40:58 -0500

    sketch

--- src/socialnet/model/twit.clj
+++ src/socialnet/model/twit.clj
@@ -5,8 +5,30 @@
             [datomic.common     :as  dcm]
             [clojure.pprint     :as   pp]
             [socialnet.util     :as util]
+            [socialnet.state    :as state]
+            [socialnet.db.datatype :as dt]
             [socialnet.db.rules :refer [defrule clear-rulebase! all-rules] :as rule]
             [socialnet.db.fn    :refer [defdbfn dbfn clear-fnbase! all-dbfn] :as fn]
             [socialnet.db.ops   :refer [entity describe] :as db]
             [socialnet.api.twitter :as twitter]
             [clojure.tools.logging :as log]))
+
+(def me  7623072)
+
+(defn ensure-twits [& more]
+  (vals (:tempids @(d/transact (db/conn)
+                               (for [ident more]
+                                 {:db/id #db/id[:db.part/user]
+                                  :dt/dt :twit
+                                  :twit/id id})))))
+
+(defn ensure-twit-id [n]
+  (first (ensure-twit-ids n)))
+
+(defn upsert-twit! [{id :id
+                     name :screen_name
+                     namestring :name
+                     lang :lang
+                     description :description
+
+                     }]
