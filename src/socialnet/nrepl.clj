d8c18a5e11b70e4bae902a6ca13e2bc523412599
Dan Lentz <danlentz@gmail.com>
2015-12-05 12:38:43 -0500

    network repl server component

--- src/socialnet/nrepl.clj
+++ src/socialnet/nrepl.clj
@@ -1,11 +1,28 @@
 (ns socialnet.nrepl
   (:require [clojure.tools.nrepl.server :as nrepl-server]
             [clojure.tools.logging      :as log]
-            [socialnet.edn              :as dedn]
+            [com.stuartsierra.component :as component]
             [cider.nrepl :refer [cider-nrepl-handler]]))
 
 
-(defn -main []
-  (nrepl-server/start-server
-    :port (dedn/config-value :nrepl)
-    :handler cider-nrepl-handler))
+(def +nrepl-handler+ cider-nrepl-handler)
+
+(defrecord NRepl [port server]
+  component/Lifecycle
+
+  (start [self]
+    (when server (component/stop self))
+    (let [s (nrepl-server/start-server
+             :port port
+             :handler +nrepl-handler+)]
+      (log/info "NREPL component started @" port)
+      (assoc self :port port :server s)))
+
+  (stop [self]
+    (when server
+      (nrepl-server/stop-server server)
+      (log/info "NREPL component stopped")
+      (assoc self :port nil :server nil))))
+
+(defn make-nrepl-server [config]
+  (map->NRepl {:port (-> config :nrepl :port)}))
