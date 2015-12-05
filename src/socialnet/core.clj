54a7bcf82a0e3b0043b426680c08553dcdd16ff3
Dan Lentz <danlentz@gmail.com>
2015-12-05 12:38:17 -0500

    refactor of stateful components

--- src/socialnet/core.clj
+++ src/socialnet/core.clj
@@ -2,19 +2,26 @@
   (:require [com.stuartsierra.component :as component])
   (:require [socialnet.nrepl :as nrepl]
             [socialnet.state :as state]
-            [socialnet.util :as util]))
-
-(defn new-system []
-  (component/system-map
-    :a-component "Add your components here"))
+            [socialnet.edn   :as dedn]
+            [socialnet.util  :as util]))
 
 (def system nil)
+
+(defn make-system
+  ([]
+   (make-system :config))
+  ([configuraton-designator]
+   (let [config (dedn/resource-value configuraton-designator nil)]
+     (component/system-map
+      :config config
+      :nrepl  (nrepl/make-nrepl-server config)))))
+
+
 
 (defn init
   "Constructs the current development system."
   []
-  (alter-var-root #'system
-    (constantly (new-system))))
+  (alter-var-root #'system (constantly (make-system))))
 
 (defn start
   "Starts the current development system."
@@ -24,15 +31,20 @@
 (defn stop
   "Shuts down and destroys the current development system."
   []
-  (alter-var-root #'system
-                  (fn [s] (when s (component/stop s)))))
+  (alter-var-root #'system (fn [s]
+                             (when s (component/stop s)))))
 
 (defn go
-  "Initializes the current development system and starts it running."
+  "Initializes and starts the current development system."
   []
   (init)
   (start))
 
+;; TODO: do? (resolve conflicting dependencies )
+
 ;; (defn reset []
 ;;   (stop)
 ;;   (refresh :after 'user/go))
+
+(defn -main []
+  (go))
